3920be5a78f7825314a70e0911c90b4705aa2d8d
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2026-01-16 13:15:17 +0300

    update to async Session db

--- database.py
+++ database.py
@@ -1,16 +1,25 @@
 # database.py
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
 from sqlalchemy.orm import sessionmaker
 from model.Chats import Base
 
 DATABASE_URL = "sqlite:///./chatapp.db"
 
-engine = create_engine(
+engine = create_async_engine(
     DATABASE_URL,
-
+    echo=True,
+    pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = async_sessionmaker(
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 def get_db():
     db = SessionLocal()
