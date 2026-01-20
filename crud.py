813a291459e0a7070464f41d38e21271fdf89338
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2026-01-20 23:42:58 +0300

    add style and middleware

--- crud.py
+++ crud.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from model.Chats import Chat, Message
 from scheme.Chats import ChatCreate, MessageCreate
 
+
 def create_chat(db: Session, chat_in: ChatCreate) -> Chat:
     chat = Chat(title=chat_in.title)
     db.add(chat)
@@ -10,8 +11,12 @@ def create_chat(db: Session, chat_in: ChatCreate) -> Chat:
     db.refresh(chat)
     return chat
 
+
 def get_chat(db: Session, chat_id: int) -> Chat | None:
     return db.query(Chat).filter(Chat.id == chat_id).first()
+
+def get_chats(db: Session, skip: int = 0, limit: int = 10) -> list[Chat]:
+    return db.query(Chat).offset(skip).limit(limit).all()
 
 def create_message(db: Session, chat_id: int, msg_in: MessageCreate) -> Message:
     msg = Message(chat_id=chat_id, text=msg_in.text)
@@ -19,6 +24,7 @@ def create_message(db: Session, chat_id: int, msg_in: MessageCreate) -> Message:
     db.commit()
     db.refresh(msg)
     return msg
+
 
 def get_message(db: Session, message_id: int) -> Message | None:
     return db.query(Message).filter(Message.id == message_id).first()
@@ -28,3 +34,11 @@ def delete_chat(db: Session, chat_id: int) -> None:
     if chat:
         db.delete(chat)
         db.commit()
+
+def put_chat(db: Session, chat_id: int, chat_in: ChatCreate) -> Chat | None:
+    chat = db.query(Chat).filter(Chat.id == chat_id).first()
+    if chat:
+        chat.title = chat_in.title
+        db.commit()
+        db.refresh(chat)
+    return chat
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
+import time
+import logging
+from datetime import datetime
+
 from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException, Request, status
 
 from model.Chats import Base
 from scheme.Chats import ChatCreate, MessageCreate, ChatRead, MessageRead
@@ -8,32 +12,90 @@ import crud
 
 app = FastAPI()
 
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = datetime.utcnow()
+
+    response = await call_next(request)
+
+    end_time = datetime.utcnow()
+    duration = (end_time - start_time).total_seconds() * 1000
+
+    logger.warning(
+        f"{request.method} {request.url.path} — "
+        f"Status: {response.status_code} — "
+        f"Duration: {duration:.2f} ms"
+    )
+
+    return response
+
 
 @app.on_event("startup")
 def startup():
     create_tables()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/chats", response_model=ChatRead)
+@app.post(
+    "/chats",
+    tags=["Chats "],
+    description="Create new chat",
+    response_model=ChatRead,
+    status_code=status.HTTP_201_CREATED,
+)
 def create_chat(chat: ChatCreate, db: Session = Depends(get_db)):
     db_chat = crud.create_chat(db, chat)
     return db_chat
 
 
-@app.get("/chats/{chat_id}", response_model=ChatRead)
-def get_chat(chat_id: int, db: Session = Depends(get_db)):
+@app.get(
+    "/chats",
+    tags=["Chats "],
+    response_model=list[ChatRead],
+    status_code=status.HTTP_200_OK,
+)
+def read_chats(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    db_chats = crud.get_chats(db, skip=skip, limit=limit)
+    if not db_chats:
+        raise HTTPException(status_code=404, detail="Chat not found")
+    return db_chats
+
+
+@app.get(
+    "/chats/{chat_id}",
+    tags=["Chats "],
+    response_model=ChatRead,
+    status_code=status.HTTP_200_OK,
+)
+def get_chat(chat_id: int, db: Session = Depends(get_db), response: Request = None):
     db_chat = crud.get_chat(db, chat_id)
+    if not db_chat:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code=404, detail="Chat not found")
+    return db_chat
+
+
+@app.put(
+    "/chats/{chat_id}",
+    tags=["Chats "],
+    response_model=ChatRead,
+    status_code=status.HTTP_200_OK,
+)
+def put_chat(chat_id: int, chat: ChatCreate, db: Session = Depends(get_db)):
+    db_chat = crud.put_chat(db, chat_id, chat)
     if not db_chat:
         raise HTTPException(status_code=404, detail="Chat not found")
     return db_chat
 
 
-@app.post("/chats/{chat_id}/messages/", response_model=MessageRead)
+@app.post(
+    "/chats/{chat_id}/messages/",
+    tags=["Chats "],
+    response_model=MessageRead,
+    status_code=status.HTTP_201_CREATED,
+)
 def create_message(chat_id: int, message: MessageCreate, db: Session = Depends(get_db)):
     chat = crud.get_chat(db, chat_id)
     if not chat:
@@ -43,12 +105,7 @@ def create_message(chat_id: int, message: MessageCreate, db: Session = Depends(g
     return db_message
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.delete("/chats/{chat_id}")
+@app.delete("/chats/{chat_id}", tags=["Chats "], status_code=status.HTTP_204_NO_CONTENT)
 def delete_chat(chat_id: int, db: Session = Depends(get_db)):
     chat = crud.delete_chat(db, chat_id)
     if not chat:
